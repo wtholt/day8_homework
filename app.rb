a64270c9459e8b722dfebcde35799795d9247d52
William Holt <wtholt27@gmail.com>
2015-01-14 16:46:28 -0500

    finished website

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
+require 'faker'
 
 get '/' do
+	@fake_paragraph = Faker::Lorem.paragraph
 	erb :index, layout: :layout
 	# require '/views/layout.erb'
+	
+
 end
 
 get '/about' do
+	@fake_paragraph = Faker::Lorem.paragraph(5)
 	erb :about, layout: :layout
 end
 
 get '/contact' do
+	@fake_paragraph = Faker::Lorem.paragraph(5)
 	erb :contact, layout: :layout
 end
+
+
+
+
