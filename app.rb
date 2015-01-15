9ce185f98cc1134adbe6962c453663be53e1af93
William Holt <wtholt27@gmail.com>
2015-01-14 20:58:53 -0500

    added erb elements

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'faker'
 
 get '/' do
+	# @background = {image_url: "/road.jpg"}
+	@image1 = {image_url: "/github.png"}
+	@image2 = {image_url: "/linkedin.png"}
 	@fake_paragraph = Faker::Lorem.paragraph
 	erb :index, layout: :layout
 	# require '/views/layout.erb'
@@ -10,11 +13,17 @@ get '/' do
 end
 
 get '/about' do
+	# @background = {image_url: "/road.jpg"}
+	@image1 = {image_url: "/github.png"}
+	@image2 = {image_url: "/linkedin.png"}
 	@fake_paragraph = Faker::Lorem.paragraph(5)
 	erb :about, layout: :layout
 end
 
 get '/contact' do
+	# @background = {image_url: "/road.jpg"}
+	@image1 = {image_url: "/github.png"}
+	@image2 = {image_url: "/linkedin.png"}
 	@fake_paragraph = Faker::Lorem.paragraph(5)
 	erb :contact, layout: :layout
 end
